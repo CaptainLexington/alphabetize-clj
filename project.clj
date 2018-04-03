08e6ab914cf2a5f07a597a753eb29e7faabac834
C. Warren Dale <dale@cwarrendale.com>
2018-04-03 15:50:26 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject alphabetize "0.1.0-SNAPSHOT"
+  :description "A library for alphabetizing strings according to literary standards."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
